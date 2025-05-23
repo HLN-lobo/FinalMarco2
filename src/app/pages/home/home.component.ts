eb62d445c4f5cb0a34211929b0a16bd304553f5f
hln-lobo <lobohenrique829@gmail.com>
2025-05-23 18:19:51 -0300

    header-footer-home styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  marcas = [
+    { nome: 'DarkLab' },
+    { nome: 'Max' },
+    { nome: 'BlackSkull' },
+    { nome: 'Growth' },
+  ]
 }
